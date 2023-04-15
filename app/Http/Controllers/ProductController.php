104b456449237f5a00782e2270ae1f002504aa25
ChristopherNapolesOneDoc <114975085+ChristopherNapolesOneDoc@users.noreply.github.com>
2023-04-15 18:19:08 +0800

    get product list

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
 
 class ProductController extends Controller
-{
-    //
+{   
+    /**
+    * method getProducts
+    * @param request
+    * return paginated products if success and error message when there is an error
+    * CPN - 04/15/2023
+    */
+    public function getProducts(Request $request){
+        try{
+            $products = DB::table('product')->paginate(5);
+            return response(['products' => $products]);
+        }catch(\Exception $e){
+            return response(['errorMessage' => $e->getMessage()]);
+        }
+    }
 }
