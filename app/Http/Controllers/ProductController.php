17887c246cb3d89943ca2c7726734e6614c916b2
ChristopherNapolesOneDoc <114975085+ChristopherNapolesOneDoc@users.noreply.github.com>
2023-04-15 18:34:52 +0800

    Added product details functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -22,5 +23,22 @@ class ProductController extends Controller
         }catch(\Exception $e){
             return response(['errorMessage' => $e->getMessage()]);
         }
+    }
+
+    /**
+    * method getProductsDetails
+    * @param request
+    * return product details if success and error message when there is an error
+    * CPN - 04/15/2023
+    */
+    public function getProductDetails(Request $request){
+        try{
+            $id = $request->productId ?? '';
+            $productDetails = Product::findOrFail($id) ?? '';
+            return response(['productDetails' => $productDetails]);
+        }catch(\Exception $e){
+            return response(['errorMessage' => $e->getMessage()]);
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('get-products',[ProductController::class,'getProducts']);
+Route::get('product-details',[ProductController::class,'getProductDetails']);
